517d2e63de61d12e377cda1c66ae4ba4bc984c57
eolso006 <eaolson@uark.edu>
2016-11-03 13:29:34 -0500

    Assign 8 JS done but can't format CSS correctly

---
+++ Assignment8/JS-Assignment/scripts/main.js
@@ -0,0 +1,11 @@
+var TodaysDate
+var HowManyCoffee
+var WearingGreen
+
+TodaysDate = "Nov 1, 2016"
+HowManyCoffee = "1"
+WearingGreen = "No"
+
+console.log(TodaysDate);
+console.log(HowManyCoffee);
+console.log(WearingGreen);
