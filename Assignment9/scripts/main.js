ec8137ae48c9458a27875452c00162ce79e93edb
eolso006 <eaolson@uark.edu>
2016-11-04 13:52:50 -0500

    assignment 8 done & 9 done

--- Assignment9/scripts/main.js
+++ Assignment9/scripts/main.js
@@ -1,24 +1,55 @@
+/*******Part 1***********/
+//Place all of your part 1 code here
+var name = "Eric Olson";
+document.getElementById('partOne').innerHTML = name;
 
+/*******Part 2***********/
+var foo = "This is a String";
+foo = "This is not an integer";
 
+document.getElementById('partTwo').innerHTML = foo;
 
+/*******Part 3***********/
+var partThree = 5;
+partThree += 10;
+partThree *= 14;
+partThree -= 25;
 
+document.getElementById('partThree').innerHTML = partThree
 
+/*******Part 4***********/
+var fName = "Eric"
+var lName = "Olson"
 
+var fullName = fName + " " + lName
 
+document.getElementById('partFour').innerHTML = fullName
 
+/*******Part 5***********/
+var myHtml = ""
+myHtml += "<h2>Title</h2>"
+myHtml += "<p>Lorem ipsum dolor</p>"
+myHtml += "<h3>Pellentesque habitant morbi</h3>"
+myHtml += "<p>Morbi in sem quis dui placerat ornare.</p>"
 
+document.getElementById('partFive').innerHTML = myHtml
 
+/*******Part 6***********/
+var part6Number =getRandomNumber()
 
+var part6Result
+  if (part6Number > 50) {
+    document.getElementById('part6Result').innerHTML = "Above 50"
+  } else if (part6Number <= 50) {
+    document.getElementById('part6Result').innerHTML = "Below or equal to 50"
+  }
 
+document.getElementById('part6Number').innerHTML = part6Number
 
-var part6Number =getRandomNumber(); 
-
-/**
- *      Part 7
- */
+/***      Part 7*/
 var part7Number = getRandomNumber();
 
-
+if (x > y && x > z)
 
 
 ////////////////////////////////////////////////////////////////
