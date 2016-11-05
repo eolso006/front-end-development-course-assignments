67cbf3753ab136862ff6f4ccfe20000e87634f4e
eolso006 <eaolson@uark.edu>
2016-11-05 17:11:31 -0500

    Assignment 10 thru part 3

--- Assignment10/JS-Assignment/scripts/main.js
+++ Assignment10/JS-Assignment/scripts/main.js
@@ -1,20 +1,47 @@
 function update() {
     //////////////////////Your Code Starts here
 
-
     ///Part 1
     var firstName = getValue('firstName');
     var lastName = getValue('lastName');
     console.log(firstName);
     console.log(lastName);
+//populate names
+var fullName = firstName + " " + lastName;
+document.getElementById('p1_fullName').innerHTML = fullName
+//validate names
+var isValidName = true;
+var notValidName = false;
+if (firstName == "" || lastName == "") {
+  getElm("p1_valid").innerHTML = notValidName
+} else {
+  getElm("p1_valid").innerHTML = isValidName
+}
+//name length
+var fullNameLength = (firstName+lastName).length;
+getElm("p1_fullNameLength").innerHTML = fullNameLength
 
-
-
+/*
+var firstNameInput = getElm('firstName');
+if (isFnameValid === false) {
+  firstNameInput.classlist.add('invalid');
+} else {
+  firstNameInput.classlist.remove('invalid');
+}*/
 
     // Part 2
+//populate email
     var emailAddress = getValue('emailAddress');
     console.log(emailAddress);
+getElm("p2_email").innerHTML = emailAddress;
 
+//validate @
+var isEmailValid = (emailAddress.indexOf('@') >= 0);
+//var isEmailValid = (emailAddress.indexOf('@') > -1)
+getElm("p2_valid").innerHTML = isEmailValid;
+
+//length
+getElm("p2_emailLength").innerHTML = emailAddress.length;
 
     // Part 3
     var randomNumber = getValue('randomNumber');
