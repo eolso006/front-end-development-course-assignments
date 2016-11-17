28ba44eb03690f4b3d138603a8976f850072ad18
eolso006 <eaolson@uark.edu>
2016-11-17 15:09:18 -0600

    struggling with 12 js

--- Assignment12/scripts/main.js
+++ Assignment12/scripts/main.js
@@ -1,24 +1,68 @@
+function submit() {
+
 function getElm(id) {
   return document.getElementById(id);
 }
-var inputs = []
+function getValue(id) {
+  return getElm(id).value;
+}
+function Ipush() {
+  return inputs.push("FIELD IS REQUIRED BEFORE SUBMISSION")
+}
+var html = getElm(message).innerHTML = '<br>';
 //var invalid =
 ///////////
 
-document.getElementById("id").innerHTML
+var fName = getValue("firstName")
 
 
+var lName = getValue("lastName")
+var email = getValue("emailAddress")
+var pword = getValue("pword")
+var Cpword = getValue("confirmPword")
+
+var inputs = []
+if (fName == "") {
+  //(Ipush + " " +
+  console.log("Please Fill In First Name");
+}
+if (lName == "") {
+  //(Ipush + " " +
+  console.log("Please Fill In Last Name");
+}
+if (email == "" || email.indexOf('@') >= 0) {
+  //(Ipush + " " +
+  console.log("Please Fill In a Valid Email");
+}
+if (pword == "") {
+  //(Ipush + " " +
+  console.log("Please Create a Password");
+}
+if (Cpword == "" && Cpword !== pword) {
+  //(Ipush + " " +
+  console.log("Passwords Do Not Match");
+}
+console.log(inputs);
 
 
-///////////
-var submit = function() {
-  alert('Submission Complete')
 };
+///////////
+var submit2 = document.getElementById("btnsubmit");
+submit2.addEventListener('click', submit);
+
+var submit2
+for (var i = 0; i < inputs.length; i++) {
+  alert('Submission Complete')
+}
+/*
 var reset = function() {
   alert('u reset doc')
 };
 getElm("btnsubmit").addEventListener('click', submit);
 getElm("btnreset").addEventListener('click', reset);
+*/
+
 
 //also throw reset function inside of input field validation to make it reset when checking for validation //
 // or something //
+/*  */
