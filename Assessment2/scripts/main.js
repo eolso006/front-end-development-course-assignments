3019f58de19498e0b69334955cb5b8cca40ff752
eolso006 <eaolson@uark.edu>
2016-11-29 17:50:47 -0600

    assessment 2 still need css but java is complete

--- Assessment2/scripts/main.js
+++ Assessment2/scripts/main.js
@@ -27,6 +27,10 @@ function addErr(input) {
   input.classList.add('errClass');
 }
 
+function addBlue(input) {
+  input.classList.add('blue-border');
+}
+
 function removeErr(input) {
   input.classList.remove('errClass');
 }
@@ -38,54 +42,69 @@ function removeErr(input) {
 //      fName.classList.add('errClass');
 }
     else {
-      removeErr(fName);
+      removeErr(fName)
+      addBlue(fName);
 //      fName.classList.remove('errClass');
     }
   var lName = getElm("lastName")
   if (lName.value === '') {
     errorMsg.push('Last Name is Required')
-    lName.classList.add('errClass');
-//  lName.addErr;
+//    lName.classList.add('errClass');
+    addErr(lName);
 }
     else {
-      lName.classList.remove('errClass');
-//      lName.removeErr;
+//      lName.classList.remove('errClass');
+      removeErr(lName)
+      addBlue(lName);
     }
   var email = getElm("emailAddress")
   if (email.value === '') {
     errorMsg.push('Email is Required')
-    email.classList.add('errClass');
+//    email.classList.add('errClass');
+      addErr(email);
 }
   else if (email.value.indexOf('@') == -1 || email.value.indexOf('.com') == -1) {
     errorMsg.push('Valid Email is Required')
-    email.classList.add('errClass');
+//    email.classList.add('errClass');
+      addErr(email);
 }
   else {
-  email.classList.remove('errClass');
+//  email.classList.remove('errClass');
+    removeErr(email)
+    addBlue(email);
   }
   var findUS = getElm("findus");
   if (findus.value === "") {
   errorMsg.push('Please tell us how you heard about us')
-  findus.classList.add('errClass')
+//  findus.classList.add('errClass')
+    addErr(findus);
 }
   else {
-    findUS.classList.remove('errClass');
+//    findUS.classList.remove('errClass');
+    removeErr(findus)
+    addBlue(findus);
   }
   var need2blend = getElm("need2blend");
   if (need2blend.value === "") {
   errorMsg.push('We need to know why you need this!')
-  need2blend.classList.add('errClass')
+//  need2blend.classList.add('errClass')
+    addErr(need2blend);
 }
   else {
-  need2blend.classList.remove('errClass');
+//  need2blend.classList.remove('errClass');
+    removeErr(need2blend)
+    addBlue(need2blend);
   }
   var terms = getElm("terms");
   if (terms.checked == false) {
   errorMsg.push('Terms and Conditions Must Be Accepted')
-  terms.classList.add('errClass')
+//  terms.classList.add('errClass')
+    addErr(terms);
 }
   else {
-  terms.classList.remove('errClass');
+//  terms.classList.remove('errClass');
+    removeErr(terms)
+    addBlue(terms);
   }
 
 console.log(errorMsg);
@@ -116,7 +135,9 @@ if (errorMsg.length > 0) {
 getElm("errorlist").innerHTML = html
 }
   else {
-  alert('Submission Complete')
+    getElm("errorlist").innerHTML = "Thank You For Your Submission"
+//    alert('Thank You For Your Submission')
+    getElm("reset").click();
 }
 
 
