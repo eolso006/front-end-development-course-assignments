f5a22925d8b03b152cf3e0fa75dc963c4f66a08f
eolso006 <eaolson@uark.edu>
2016-12-06 20:10:08 -0600

    assign 17 done

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+  $(".error").hide();
 
+var inputG = $('.input-group')
+  inputG.filter(':even').addClass('even');
 
+  $('input').on('blur', function(){
+  	var input = $(this);
+    var msg = input.closest('.input-group').find('.requiredMsg')
+  	if(input.val() === ''){
+      msg.fadeIn();
+
+    }else{
+      msg.fadeOut();
+    }
+})
+    $('#emailAddress').on('blur', function(){
+      var input = $(this);
+      var inputValue = input.val();
+      var emailMsg = input.closest('.input-group').find('.emailMsg');
+
+      if (inputValue.indexOf('@') == -1 || inputValue.indexOf('.com') == -1 ) {
+        emailMsg.fadeIn();
+      }else {
+        emailMsg.fadeOut();
+      }
+    })
 
 });
