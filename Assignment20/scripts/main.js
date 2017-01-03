e0c11295faa724ac062bf31a00530128fa3735e6
eolso006 <eaolson@uark.edu>
2017-01-03 17:43:14 -0600

    assign 20 kinks in JS

--- Assignment20/scripts/main.js
+++ Assignment20/scripts/main.js
@@ -1,4 +1,36 @@
 $(document).ready(function () {
+  $('.error').hide();
+
+
+
 
 
 });
+
+
+
+$('input, textarea').on('blur', function(){
+var input = $(this);
+var msg = input.closest('.input-group').find('.requiredMsg')
+if(input.val() === ''){
+  input.addClass('invalid');
+  msg.fadeIn();
+
+}else{
+  msg.fadeOut();
+  input.removeClass('invalid')
+  input.addClass('valid');
+}
+})
+/*   var val = input.val();
+
+    //Find the required input span.
+    var msgSpan = input.closest('.input-group').find('.requiredMsg');
+    if (val === '') {
+        msgSpan.fadeIn();
+        input.addClass('invalid');
+    } else {
+        msgSpan.fadeOut();
+        input.removeClass('invalid');
+    } */
+/* */
