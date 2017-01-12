d1f2117b3d2ea3233703dab16ca8092e29e43c19
eolso006 <eaolson@uark.edu>
2017-01-11 19:58:28 -0600

    assign 22 progress

--- Assignment20/scripts/main.js
+++ Assignment20/scripts/main.js
@@ -2,25 +2,27 @@ $(document).ready(function () {
   $('.error').hide();
 
   $('#btnSubmit').on('click', function () {
-          validateAllInputs();
+          validateAllInputs()
       });
 
-/*
+console.log(validateAllInputs);
+
       $('#btnReset').on('click', function () {
           var allInputs = $('input, textarea')
           allInputs.val('');
           allInputs.removeClass('invalid');
           $('.error').hide();
-          $('#messages').html('');
       });
-*/
 
 
 });
+
+
 
 $('input, textarea').on('blur', function(){
 var input = $(this);
-var msg = input.closest('.input-group').find('.requiredMsg')
+validateInput(input);
+/* var msg = input.closest('.input-group').find('.requiredMsg')
 if(input.val() === ''){
   input.addClass('invalid');
   msg.fadeIn();
@@ -29,32 +31,70 @@ if(input.val() === ''){
   msg.fadeOut();
   input.removeClass('invalid')
   input.addClass('valid');
-}
+} */
 });
 
 
-/*   $('input, textarea').on('blur', function(){
-var input = $(this);
-var msg = input.closest('.input-group').find('.requiredMsg')
-if(input.val() === ''){
-  input.addClass('invalid');
-  msg.fadeIn();
-
-}else{
-  msg.fadeOut();
-  input.removeClass('invalid')
-  input.addClass('valid');
+function validateInput(input) {
+  var msg = input.closest('.input-group').find('.requiredMsg')
+  if(input.val() === ''){
+    input.addClass('invalid');
+    msg.fadeIn();
+    return false;
+  }else{
+    msg.fadeOut();
+    input.removeClass('invalid')
+    input.addClass('valid');
+    return true;
+  }
 }
-});
 
-var msgSpan = input.closest('.input-group').find('.requiredMsg');
-    if (val === '') {
-        msgSpan.fadeIn();
-        input.addClass('invalid');
+
+function validateAllInputs() {
+    //Select all inputs and text areas
+    var allInputs = $('input, textarea');
+    console.log(allInputs);
+
+    //Loop through each and validate individually
+    for (var i = 0; i < allInputs.length; i++) {
+        var currentInput = allInputs.eq(i);
+        console.log(i);
+      }
+}
+
+var answerArray = ["URname", "URanswer"];
+var name = $("#name").val
+var answer = $("#answer").val
+console.log(answer);
+function posted() {
+  if (validateInput = true) {
+
+  }
+}
+
+
+var html = '';
+
+
+/*
+'<div class="answerBox"><p>$("#answer").val</p><ul><li><img src="img/user.png" alt="user icon" style="width:64px;height:64px;"></li><li>$("#name").val</li></ul></div>'
+
+var inputs = ['firstName', 'lastName'];
+var html = '';
+    if (errorList.length > 0) {
+        //Loop through and add each message to our html variable
+        html = '<ul class="errors">';
+        for (var i = 0; i < errorList.length; i++) {
+            //html += errorMsg[i] + '<br />';
+            html = html + '<li>' +  errorList[i] + '</li>';
+        }
+        html += "</ul>";
     } else {
-        msgSpan.fadeOut();
-        input.removeClass('invalid');
+        html = '<h1>Thank you for registering</h1>';
+        reset();
     }
-*/
+
+    getE('messages').innerHTML = html;
+ */
 
 /* */
