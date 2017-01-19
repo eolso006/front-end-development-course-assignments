a05b80a2ddb7a9fbe086cd777dc13527e93e8543
eolso006 <eaolson@uark.edu>
2017-01-19 17:30:56 -0600

    start assign 25 nv grid framework

---
+++ Assignment25/scripts/main.js
@@ -0,0 +1,7 @@
+// TODO:
+$(document).ready(function(){
+// TODO: add toggles and slides to fontawesome button
+
+// TODO: add toggle, change this.closest from nav-bar to dropdown and change find nav items (create) to sub-menu
+
+})
